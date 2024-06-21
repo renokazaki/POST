92928708e8b704138e8d35b258ec8ca542feca6b
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-06-21 17:05:41 +0900

    css fixed for iphone at 0621 1705

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
       <div className="container">
         {messages.map((message, index) => (
           <div key={index}>
-            <p
+            <div
               className={
                 message.userId !== myId
                   ? "other-user-message"
@@ -85,7 +85,7 @@ const App = () => {
               }
             >
               {message.userName}: {message.text}
-            </p>
+            </div>
           </div>
         ))}
       </div>
